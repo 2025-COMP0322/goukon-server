2a2c9a6421b6a802d5dfd98e3eb37779a9b39702
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-10-12 18:06:35 +0900

    Feat: Token 도메인 추가 (#1)

---
+++ src/main/java/com/kr/knucampus/domain/token/RefreshToken.java
@@ -0,0 +1,39 @@
+package com.kr.knucampus.domain.token;
+
+import com.kr.knucampus.global.utils.DateUtils;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public final class RefreshToken implements Token {
+    private final SecretKey secretKey;
+    private final Long expiredTime;
+
+    public RefreshToken(
+            @Value("${jwt.refresh.secret}") String secretKey,
+            @Value("${jwt.refresh.expired}") Long expiredTime
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.expiredTime = expiredTime;
+    }
+
+    @Override
+    public TokenType getType() {
+        return TokenType.REFRESH;
+    }
+
+    @Override
+    public SecretKey secretKey() {
+        return this.secretKey;
+    }
+
+    @Override
+    public Date getExpiredTime() {
+        return DateUtils.convertToDate(this.expiredTime);
+    }
+}
