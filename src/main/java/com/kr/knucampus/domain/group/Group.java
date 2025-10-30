07e77c492f81cd789745fd0432592c5cadaaba16
jbh010204 <jbh010204@naver.com>
2025-10-30 11:02:15 +0900

    Feat: Group 엔티티에 Builder 패턴 추가

--- src/main/java/com/kr/knucampus/domain/group/Group.java
+++ src/main/java/com/kr/knucampus/domain/group/Group.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,5 +24,10 @@ public class Group {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @Builder
+    public Group(Long id, Gender gender) {
+        this.id = id;
+        this.gender = gender;
+    }
 }
 
