9d168cd5cfa3f5b6e46e028db81e6df76bbea302
jbh010204 <jbh010204@naver.com>
2025-10-30 11:11:47 +0900

    Feat: ChatRoom 엔티티 추가

---
+++ src/main/java/com/kr/knucampus/domain/chatroom/ChatRoom.java
@@ -0,0 +1,36 @@
+package com.kr.knucampus.domain.chatroom;
+
+import com.kr.knucampus.domain.matchingsession.MatchingSession;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "session_id", unique = true)
+    private MatchingSession session;
+
+    private ChatRoom(MatchingSession session) {
+        this.session = session;
+    }
+
+    public static ChatRoom create(MatchingSession session) {
+        return new ChatRoom(session);
+    }
+}
+
