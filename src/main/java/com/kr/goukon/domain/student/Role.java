f58eae26cbe88174165ea83a6821b31e6a48802d
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-12-10 22:23:13 +0900

    feat: Role 메서드 추가

--- src/main/java/com/kr/goukon/domain/student/Role.java
+++ src/main/java/com/kr/goukon/domain/student/Role.java
@@ -1,6 +1,26 @@
 package com.kr.goukon.domain.student;
 
+import com.kr.goukon.global.exception.BusinessException;
+import com.kr.goukon.global.exception.ErrorCode;
+
 public enum Role {
     USER,   // 일반 사용자
     ADMIN   // 관리자
+    ;
+
+    public static Role convert(String role) {
+        if(USER.name().equalsIgnoreCase(role)) {
+            return USER;
+        }else if(ADMIN.name().equalsIgnoreCase(role)) {
+            return ADMIN;
+        }
+        throw new BusinessException(ErrorCode.CANNOT_CONVERT_ROLE);
+    }
+
+    public static boolean verify(Role role, Role standardRole) {
+        if(role.equals(Role.ADMIN)){
+            return true;
+        }
+        return role.equals(Role.USER) && role.equals(standardRole);
+    }
 }
